ab3165111cbb5f50940ad1b11f68b23ab9cff3d9
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-03 17:52:50 -0500

    feat: add font awesome icons to project

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,21 @@
 import { createApp } from "vue";
+import App from "./App.vue";
 import "./style.css";
 import io from "socket.io-client";
+// Vue router  imports
 import { createRouter } from "vue-router";
-import App from "./App.vue";
 import router from "./router";
+// Font Awesome imports
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 const socket = io("http://localhost:3005");
 
+library.add(faPaperPlane);
+
 const app = createApp(App);
 app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.provide("socket", socket);
 app.mount("#app");
