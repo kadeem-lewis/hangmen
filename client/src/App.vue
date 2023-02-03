6c9c1c5efa3051f3410a4cecfb257b9a899a0ef6
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-03 14:22:25 -0500

    style: center container on page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container text-white min-h-screen">
+  <div class="container text-white min-h-screen m-auto">
     <NavBar></NavBar>
     <main>
       <RouterView></RouterView>
