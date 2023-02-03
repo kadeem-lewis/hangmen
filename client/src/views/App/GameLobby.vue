746e32afcf284df90be0ed9c888e08d4fcee223c
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-03 17:53:45 -0500

    style: style game area

--- client/src/views/App/GameLobby.vue
+++ client/src/views/App/GameLobby.vue
@@ -1,6 +1,24 @@
 <template>
-  <h1>Choose Mode</h1>
+  <div>
+    <p class="text-center text-3xl">Waiting</p>
+  </div>
+  <div></div>
   <ul>
     <li>Player 1</li>
+    <li>Player 2</li>
+    <li>Player 3</li>
+    <li>Player 4</li>
   </ul>
+  <div>
+    <button
+      class="rounded-full bg-red-600 py-2 hover:bg-red-500 m-2 font-semibold text-xl w-1/2"
+    >
+      Not Ready
+    </button>
+    <button
+      class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/2"
+    >
+      Start
+    </button>
+  </div>
 </template>
