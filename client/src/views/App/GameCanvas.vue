23b5146c1183ef00a5c4d2da0913fb398205d7b6
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-03 19:05:17 -0500

    style: style game area buttons

--- client/src/views/App/GameCanvas.vue
+++ client/src/views/App/GameCanvas.vue
@@ -7,9 +7,21 @@
     <p class="text-center text-3xl">Player's Turn</p>
   </div>
   <div>The Game</div>
-  <div>
-    <button>Guess Letter</button>
-    <button>Guess Word</button>
-    <button>Skip Turn</button>
+  <div class="flex">
+    <button
+      class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/3"
+    >
+      Guess Letter
+    </button>
+    <button
+      class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/3"
+    >
+      Guess Word
+    </button>
+    <button
+      class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/3"
+    >
+      Skip Turn
+    </button>
   </div>
 </template>
