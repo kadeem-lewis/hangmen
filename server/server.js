d779e943ca92fc7fdfdf497121245ad96c508043
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-21 02:26:29 -0500

    fix: remove comments and add leave room event to within join room

--- server/server.js
+++ server/server.js
@@ -29,46 +29,42 @@ io.on("connection", (socket) => {
   console.log(socket.id);
   socket.on("register", (username, userId) => {
     users[socket.id] = new User(username, userId);
-    // const user = new User(username, userId, socket.id);
-    // users.push(user);
   });
 
   socket.on("request-room-code", () => {
     const room = new Room();
     activeRooms[room.code] = room;
-    // activeRooms.push(room);
     io.emit("create-room", room.code);
   });
   socket.on("join-room", (roomCode, cb) => {
-    //let foundRoom = activeRooms.find((room) => room.code === roomCode);
     if (activeRooms[roomCode]) {
-      //currentUser = users.find((user) => user.socketId === socket.id);
       socket.join(roomCode);
       activeRooms[roomCode].addPlayer(users[socket.id]);
-      //not emitting to everyone
-      io.to(roomCode).emit("new-player", users[socket.id]);
+      users[socket.id].currentRoom = roomCode;
+      io.to(roomCode).emit(
+        "new-player",
+        users[socket.id],
+        activeRooms[roomCode].players
+      );
 
-      console.log(`user joined room ${roomCode}`);
       console.log(activeRooms[roomCode]);
     } else {
       cb("Game not found");
     }
 
     socket.on("send-message", (id, text) => {
-      const message = new Message(sender, text);
-      //TODO: add new message to the message array
-      console.log("messages" + messages);
-      //not emitting to everyone
       io.to(roomCode).emit("receive-message", {
         id: id,
         sender: users[socket.id].username,
         text: text,
       });
     });
-  });
-  socket.on("leave-room", () => {
-    delete activeRooms[roomCode];
-    socket.leave(roomCode);
+
+    socket.on("leave-room", () => {
+      delete activeRooms[roomCode];
+      users[socket.id].currentRoom = null;
+      socket.leave(roomCode);
+    });
   });
   socket.on("disconnect", () => {
     console.log("client disconnected");
