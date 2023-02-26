1a88c83fb00b5e3eb2e09b4d2be12c006a72902d
kadeem-lewis <print.helloworld@genericindividual.com>
2023-02-26 18:51:22 -0500

    feat: cont attempting to clear all user data on room leave

--- client/src/components/App/PlayerList.vue
+++ client/src/components/App/PlayerList.vue
@@ -1,7 +1,34 @@
 <template>
-  <div></div>
+  <ul class="grid grid-cols-2">
+    <li class="p-2 text-center" v-for="player in players" :key="player.id">
+      {{ player.username }}
+    </li>
+  </ul>
 </template>
 
 <script>
-export default {};
+import SocketIoService from "../../services/SocketIoService";
+export default {
+  mounted() {
+    this.socket = SocketIoService.setupSocketConnection();
+    this.socket.on("new-player", (player, players) => {
+      this.players = players;
+    });
+  },
+  updated() {
+    this.socket.on("player-leave-room", (user) => {
+      this.players = this.players.filter(
+        (player) => player.username !== user.username
+      );
+    });
+  },
+  data() {
+    return {
+      players: [],
+    };
+  },
+  beforeUnmount() {
+    this.players = [];
+  },
+};
 </script>
