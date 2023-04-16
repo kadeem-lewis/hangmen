9397be30e80e5cf7f2b02b9ed1f74ebf28b11649
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-16 03:17:35 -0400

    removed fontawesome from project

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,21 +5,8 @@ import "./style.css";
 import router from "./router";
 //Socket io connection
 import SocketPlugin from "./plugins/SocketPlugin";
-// Font Awesome imports
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import { faClock } from "@fortawesome/free-solid-svg-icons";
-import { faCopy } from "@fortawesome/free-solid-svg-icons";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faPaperPlane);
-library.add(faClock);
-library.add(faCopy);
-library.add(faCheck);
 
 const app = createApp(App);
 app.use(router);
 //app.use(SocketPlugin);
-app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
