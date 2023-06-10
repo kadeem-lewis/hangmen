c4f45117e26c15036f6a47233df15ebfd1ce5e0b
kadeem-lewis <print.helloworld@genericindividual.com>
2023-06-10 19:31:14 -0400

    created a layout component for app

---
+++ client/src/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container text-white min-h-screen m-auto">
+    <NavBar />
+    <main class="mt-16 max-w-md md:max-w-3xl m-auto">
+      <slot />
+    </main>
+  </div>
+</template>
