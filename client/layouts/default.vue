7f71b5e1307bb26a418cf2c5aadeee4f80ae4aba
kadeem-lewis <print.helloworld@genericindividual.com>
2023-07-06 01:36:28 -0400

    deleted layout since app only uses singular layout

--- client/layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="container text-white min-h-screen m-auto">
-    <NavBar />
-    <main class="mt-16 max-w-md md:max-w-3xl m-auto">
-      <slot />
-    </main>
-  </div>
-</template>
