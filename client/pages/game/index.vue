c26f7ea5878f76a3e37e3b01072a62e330414100
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-03 20:56:18 -0400

    updated component names to work with nuxt autoimport

--- client/pages/game/index.vue
+++ client/pages/game/index.vue
@@ -2,14 +2,13 @@
   <div class="grid grid-cols-4 gap-2">
     <div class="col-span-3">
       <NuxtPage />
-      <InputArea />
+      <AppInputArea />
     </div>
-    <ChatBox class="h-full grid-col-span-1" />
+    <AppChatBox class="h-full grid-col-span-1" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
 import SocketIoService from "../../services/SocketIoService";
 import { Socket } from "socket.io-client";
 
