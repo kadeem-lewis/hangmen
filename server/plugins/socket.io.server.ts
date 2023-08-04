d32697545b4e4f243c28c0603000d04ad85cc61f
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-04 00:16:56 -0400

    began defining socket methods

--- server/plugins/socket.io.server.ts
+++ server/plugins/socket.io.server.ts
@@ -1,14 +1,15 @@
 import { Server } from "socket.io";
 
 export default defineNitroPlugin((nitroApp) => {
-  const socketServer = new Server(useRuntimeConfig().public.socketPort, {
+  const io = new Server(useRuntimeConfig().public.socketPort, {
     serveClient: false,
     cors: {
       origin: "*",
     },
   });
 
-  socketServer.on("connection", (socket) => {
-    socket.emit("");
+  io.on("connection", (socket) => {
+    console.log(`${socket.id} is getting litty`);
+    socket.on(SocketEvent.register, (username, userId) => {});
   });
 });
