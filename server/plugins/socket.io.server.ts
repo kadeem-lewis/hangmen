e34a5453ed4f8d58946dbb92fedbc66189d8ef93
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-07 05:52:54 -0400

    updated event names and fixed leave room events

--- server/plugins/socket.io.server.ts
+++ server/plugins/socket.io.server.ts
@@ -22,23 +22,23 @@ export default defineNitroPlugin((nitroApp) => {
 
   io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
-    socket.on(ClientEvents.register, (username, userId) => {
+    socket.on(ClientEvents.REGISTER, (username, userId) => {
       users[socket.id] = new User(username, userId);
     });
 
-    socket.on(ClientEvents.request_room_code, () => {
+    socket.on(ClientEvents.REQUEST_ROOM_CODE, () => {
       const room = new Room();
       activeRooms[room.code] = room;
-      io.emit(ServerEvents.create_room, room.code);
+      socket.emit(ServerEvents.CREATE_ROOM, room.code);
     });
-    socket.on(ClientEvents.join_room, (roomCode, callback) => {
+    socket.on(ClientEvents.JOIN_ROOM, (roomCode, callback) => {
       let status = false;
       if (activeRooms[roomCode]) {
         socket.join(roomCode);
         activeRooms[roomCode].addPlayer(socket.id, users[socket.id]);
         users[socket.id].currentRoom = roomCode;
         io.to(roomCode).emit(
-          ServerEvents.new_player,
+          ServerEvents.NEW_PLAYER,
           users[socket.id],
           activeRooms[roomCode].players
         );
@@ -48,27 +48,41 @@ export default defineNitroPlugin((nitroApp) => {
         status = false;
       }
       callback({ status });
-
-      socket.on(ClientEvents.leave_room, (callback) => {
-        //! on leave room remove the player from the room, remove the room from the players current room and delete the room if the player count is 0.
+    });
+    socket.on(ClientEvents.LEAVE_ROOM, (roomCode, callback) => {
+      //! on leave room remove the player from the room, remove the room from the players current room and delete the room if the player count is 0.
+      if (
+        roomCode in activeRooms &&
+        socket.id in activeRooms[roomCode].players
+      ) {
         delete activeRooms[roomCode].players[socket.id];
         users[socket.id].currentRoom = "";
-        if (activeRooms[roomCode].players.length === 0) {
+        if (Object.keys(activeRooms[roomCode].players).length === 0) {
           delete activeRooms[roomCode];
         }
         socket.leave(roomCode);
-        if (!(socket.id in activeRooms[roomCode].players)) {
+        if (
+          !(
+            roomCode in activeRooms &&
+            socket.id in activeRooms[roomCode].players
+          )
+        ) {
           callback({
             status: "ok",
           });
         }
-        io.emit(ServerEvents.player_leave_room, users[socket.id]);
-      });
+        io.to(roomCode).emit(ServerEvents.PLAYER_LEAVE_ROOM, users[socket.id]);
+      } else {
+        callback({
+          status: "error",
+          message: "Error leaving the room. Room or player not found.",
+        });
+      }
     });
-    socket.on(ClientEvents.rejoin_room, (roomCode, cb) => {});
-    socket.on(ClientEvents.send_message, (id, text, roomCode) => {
+    socket.on(ClientEvents.REJOIN_ROOM, (roomCode, cb) => {});
+    socket.on(ClientEvents.SEND_MESSAGE, (id, text, roomCode) => {
       //this works fine for now but I need to add a timestamp and determine if to keep io functionality or not
-      io.to(roomCode).emit(ServerEvents.receive_message, {
+      io.to(roomCode).emit(ServerEvents.RECEIVE_MESSAGE, {
         id,
         sender: users[socket.id].username,
         text,
