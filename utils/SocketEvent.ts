af25db124d5258b9cf772fd9e68db5f0b1940ecc
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-06 22:03:26 -0400

    separated events into client events and server events

--- utils/SocketEvent.ts
+++ utils/SocketEvent.ts
@@ -1,10 +1,15 @@
 export enum ServerEvents {
+  create_room = "create-room",
+  new_player = "new_player",
+  receive_message = "receive_message",
+  disconnect = "disconnect",
+  player_leave_room = "player-leave-room",
+}
+export enum ClientEvents {
   register = "register",
   request_room_code = "request-room-code",
   join_room = "join-room",
   send_message = "send-message",
   leave_room = "leave room",
   rejoin_room = "rejoin-room",
-  disconnect = "disconnect",
 }
-export enum ClientEvents {}
