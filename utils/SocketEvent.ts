051dbb6f1e8e86dc5f073d2fd3cf5134a07566f8
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-07 05:53:10 -0400

    updated event names to be more easily readable

--- utils/SocketEvent.ts
+++ utils/SocketEvent.ts
@@ -1,15 +1,14 @@
 export enum ServerEvents {
-  create_room = "create-room",
-  new_player = "new-player",
-  receive_message = "receive-message",
-  disconnect = "disconnect",
-  player_leave_room = "player-leave-room",
+  CREATE_ROOM = "create-room",
+  NEW_PLAYER = "new-player",
+  RECEIVE_MESSAGE = "receive-message",
+  PLAYER_LEAVE_ROOM = "player-leave-room",
 }
 export enum ClientEvents {
-  register = "register",
-  request_room_code = "request-room-code",
-  join_room = "join-room",
-  send_message = "send-message",
-  leave_room = "leave-room",
-  rejoin_room = "rejoin-room",
+  REGISTER = "register",
+  REQUEST_ROOM_CODE = "request-room-code",
+  JOIN_ROOM = "join-room",
+  SEND_MESSAGE = "send-message",
+  LEAVE_ROOM = "leave-room",
+  REJOIN_ROOM = "rejoin-room",
 }
