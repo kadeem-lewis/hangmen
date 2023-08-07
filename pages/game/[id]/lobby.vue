72c5800bcfe8e774ab977cb756c814ae4c67dd6b
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-06 22:04:00 -0400

    created a component for game settings and added it to lobby

--- pages/game/[id]/lobby.vue
+++ pages/game/[id]/lobby.vue
@@ -1,35 +1,38 @@
 <template>
   <div>
-    <p class="text-center text-3xl">Waiting...</p>
-  </div>
-  <div>
-    <p class="font-bold text-lg">Room Code:</p>
-    <div class="py-2 text-2xl">
-      <span class="p-2 rounded-lg bg-dark-mode-400 mr-2 font-bold">{{
-        roomCode
-      }}</span>
+    <AppGameSettings />
+    <div>
+      <p class="text-center text-3xl">Waiting...</p>
+    </div>
+    <div>
+      <p class="font-bold text-lg">Room Code:</p>
+      <div class="py-2 text-2xl">
+        <span class="p-2 rounded-lg bg-dark-mode-400 mr-2 font-bold">{{
+          roomCode
+        }}</span>
+        <button
+          @click="copyCode()"
+          class="rounded-lg p-2 bg-sky-600 hover:bg-sky-500"
+        >
+          <ClipboardIcon v-if="!isCopied" class="h-6 w-6" />
+          <ClipboardDocumentCheckIcon v-else class="h-6 w-6" />
+        </button>
+      </div>
+    </div>
+    <AppPlayerList />
+    <div class="flex">
       <button
-        @click="copyCode()"
-        class="rounded-lg p-2 bg-sky-600 hover:bg-sky-500"
+        class="rounded-full bg-red-600 py-2 hover:bg-red-500 m-2 font-semibold text-xl w-1/2"
       >
-        <ClipboardIcon v-if="!isCopied" class="h-6 w-6" />
-        <ClipboardDocumentCheckIcon v-else class="h-6 w-6" />
+        Not Ready
+      </button>
+      <button
+        @click="startGame"
+        class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/2"
+      >
+        Start
       </button>
     </div>
-  </div>
-  <AppPlayerList />
-  <div class="flex">
-    <button
-      class="rounded-full bg-red-600 py-2 hover:bg-red-500 m-2 font-semibold text-xl w-1/2"
-    >
-      Not Ready
-    </button>
-    <button
-      @click="startGame"
-      class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/2"
-    >
-      Start
-    </button>
   </div>
 </template>
 
