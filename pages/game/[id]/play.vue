ac3df83d3a90566b2a9a9df71b4a41f0a2159737
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-16 02:59:58 -0400

    began defining boilerplate code for game

--- pages/game/[id]/play.vue
+++ pages/game/[id]/play.vue
@@ -2,27 +2,35 @@
   <div class="flex flex-col">
     <div>
       <span> <Icon name="heroicons:clock-solid" size="1.5rem" /></span>
-      <span>30s</span>
+      <span>{{ "30" }}s</span>
     </div>
-    <p class="text-center text-3xl">Player's Turn</p>
+    <p class="text-center text-3xl">{{ "Player's" }} Turn</p>
   </div>
   <div>The Game</div>
   <div class="flex">
     <button
       class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/3"
+      @click="guessLetter"
     >
       Guess Letter
     </button>
     <button
       class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/3"
+      @click="guessWord"
     >
       Guess Word
     </button>
     <button
       class="rounded-full bg-blue-600 py-2 hover:bg-blue-500 m-2 font-semibold text-xl w-1/3"
+      @click="skipTurn"
     >
       Skip Turn
     </button>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { $io } = useNuxtApp();
+const guessLetter = () => {};
+const guessWord = () => {};
+const skipTurn = () => {};
+</script>
