65da9c4f33e77f36d89fbd66680ba58383d38769
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-20 02:19:50 -0400

    created is ready field and reset method for user

--- packages/server/src/classes/User.ts
+++ packages/server/src/classes/User.ts
@@ -3,10 +3,19 @@ export class User {
   userId: string;
   isHost: boolean;
   currentRoom: string;
+  isReady: boolean;
+
   constructor(username: string, userId: string) {
     this.username = username;
     this.userId = userId;
     this.isHost = false;
+    this.currentRoom = "";
+    this.isReady = false;
+  }
+
+  resetUser() {
+    this.isHost = false;
+    this.isReady = false;
     this.currentRoom = "";
   }
 }
