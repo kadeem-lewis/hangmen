bc65b64db680a59fe3f65f83442e45a8016fefd4
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-21 03:08:33 -0400

    began styling playerlist to better represent game

--- packages/client/components/App/PlayerList.vue
+++ packages/client/components/App/PlayerList.vue
@@ -1,9 +1,24 @@
 <template>
   <ul class="grid grid-cols-2">
-    <li class="p-2 text-center" v-for="player in players" :key="player.userId">
-      {{ player.username }}
+    <li class="p-2 text-center" v-for="(slot, index) in 4" :key="index">
+      <div v-if="players[index]">
+        <Icon v-if="players[index].isHost" name="mdi:crown" />
+        {{ players[index].username }}
+        <Icon
+          v-if="players[index].isReady"
+          name="material-symbols:check-circle"
+        />
+      </div>
+      <div v-else>
+        Waiting for player
+
+        <button @click="removePlayerSlot">X</button>
+      </div>
+      <!-- TODO: add way to check if the current person is the host and let them see different things on screen -->
     </li>
   </ul>
+  <!-- Add a player slot button -->
+  <button v-if="players.length < 4" @click="addPlayerSlot">Add Player</button>
 </template>
 
 <script setup lang="ts">
@@ -13,6 +28,7 @@ type Player = {
   userId: string;
   username: string;
   isHost: boolean;
+  isReady: boolean;
   currentRoom: string;
 };
 
@@ -26,7 +42,23 @@ onMounted(() => {
   $io.on(ServerEvents.PLAYER_LEAVE_ROOM, ({ userId }) => {
     players.value = players.value.filter((p) => p.userId !== userId);
   });
+  $io.on(ServerEvents.READY_PLAYERS, (playersList) => {});
 });
+
+// Function to remove a player slot
+const removePlayerSlot = () => {
+  if (players.value.length > 0) {
+    // emit the event to server to reduce the max size of the room if required.
+  }
+};
+
+// Function to add a player slot
+const addPlayerSlot = () => {
+  if (players.value.length < 4) {
+    // Do something to add a new player or just increase the size of the room on the server.
+  }
+};
+
 onBeforeUnmount(() => {
   $io.off(ServerEvents.NEW_PLAYER);
   $io.off(ServerEvents.PLAYER_LEAVE_ROOM);
