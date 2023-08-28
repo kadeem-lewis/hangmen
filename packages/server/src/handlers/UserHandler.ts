1af5e4f9ba4e016f2553e340df4326e5483bda08
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-28 16:23:36 -0400

    added imports for inter server events and socket data

--- packages/server/src/handlers/UserHandler.ts
+++ packages/server/src/handlers/UserHandler.ts
@@ -3,6 +3,8 @@ import {
   ServerEvents,
   ClientPayloads,
   ServerPayloads,
+  InterServerEvents,
+  SocketData,
 } from "@hangmen/shared";
 import { User } from "../classes/User.js";
 import { Socket, Server } from "socket.io";
@@ -14,8 +16,8 @@ interface Users {
 export const users: Users = {};
 
 export const userHandler = (
-  io: Server<ClientPayloads, ServerPayloads>,
-  socket: Socket<ClientPayloads, ServerPayloads>
+  io: Server<ClientPayloads, ServerPayloads, InterServerEvents, SocketData>,
+  socket: Socket<ClientPayloads, ServerPayloads, InterServerEvents, SocketData>
 ) => {
   socket.on(ClientEvents.REGISTER, (username, userId) => {
     users[socket.id] = new User(username, userId); //TODO: client probably shouldn't be emitting id, look into the cookie thing or generate the id server side
