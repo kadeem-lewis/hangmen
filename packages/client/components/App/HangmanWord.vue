4eb9cdc47215d571357db205e2b0e77b56910be4
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-29 02:17:39 -0400

    created a component to store the hangman word

---
+++ packages/client/components/App/HangmanWord.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-center gap-1 text-2xl font-bold uppercase">
+    <span
+      v-for="(letter, index) in word"
+      :key="index"
+      class="border-b-2 border-solid border-current"
+    >
+      <span :class="guessedLetters.includes(letter) ? 'visible' : 'invisible'">
+        {{ letter }}</span
+      >
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+const word = ref("soon");
+const guessedLetters = ref(["s", "n"]);
+</script>
