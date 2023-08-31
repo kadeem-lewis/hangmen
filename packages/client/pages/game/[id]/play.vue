830e2a599d3c566932e422208e1d8a3ffee97414
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-31 02:10:00 -0400

    deleted modal in favor of nuxtui modal

--- packages/client/pages/game/[id]/play.vue
+++ packages/client/pages/game/[id]/play.vue
@@ -3,18 +3,24 @@
     <AppHangmanWord />
     <AppHangmanCanvas />
     <div class="flex">
-      <button
-        class="m-2 w-1/3 rounded-full bg-blue-600 py-2 text-xl font-semibold hover:bg-blue-500"
-        @click="guessLetter"
-      >
-        Guess Letter
-      </button>
-      <button
-        class="m-2 w-1/3 rounded-full bg-blue-600 py-2 text-xl font-semibold hover:bg-blue-500"
-        @click="guessWord"
-      >
-        Guess Word
-      </button>
+      <div>
+        <button
+          class="m-2 w-1/3 rounded-full bg-blue-600 py-2 text-xl font-semibold hover:bg-blue-500"
+          @click="letterClick = true"
+        >
+          Guess Letter
+        </button>
+        <UModal v-model="letterClick">Hello</UModal>
+      </div>
+      <div>
+        <button
+          class="m-2 w-1/3 rounded-full bg-blue-600 py-2 text-xl font-semibold hover:bg-blue-500"
+          @click="wordClick = true"
+        >
+          Guess Word
+        </button>
+        <UModal v-model="wordClick">Howdy</UModal>
+      </div>
       <button
         class="m-2 w-1/3 rounded-full bg-blue-600 py-2 text-xl font-semibold hover:bg-blue-500"
         @click="skipTurn"
@@ -26,6 +32,10 @@
 </template>
 <script setup lang="ts">
 const { $io } = useNuxtApp();
+
+const letterClick = ref(false);
+const wordClick = ref(false);
+
 const guessLetter = () => {};
 const guessWord = () => {};
 const skipTurn = () => {};
