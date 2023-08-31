71ffc4de950f1e4102228420d912c0579c2155fb
kadeem-lewis <print.helloworld@genericindividual.com>
2023-08-31 17:27:52 -0400

    updated navbar styling to amount for svg

--- packages/client/components/NavBar.vue
+++ packages/client/components/NavBar.vue
@@ -1,8 +1,9 @@
 <template>
-  <header class="flex items-center justify-between md:justify-center">
+  <header class="m-auto flex max-w-7xl items-center justify-between px-8">
     <NuxtLink to="/">
       <img src="../assets/logo.svg" alt="Hangman Logo" class="h-12" />
     </NuxtLink>
+    <ThemeToggle />
   </header>
 </template>
 <script setup lang="ts"></script>
