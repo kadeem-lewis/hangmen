1227ecd8f0485312304ca80d39c87d2445f41994
kadeem-lewis <print.helloworld@genericindividual.com>
2023-09-02 04:31:23 -0400

    began defining basic structure for game events to emit

--- packages/client/pages/game/[id]/play.vue
+++ packages/client/pages/game/[id]/play.vue
@@ -5,30 +5,36 @@
     <div class="flex justify-between">
       <div>
         <button
-          class="rounded-full bg-blue-600 px-4 py-2 text-xl font-semibold hover:bg-blue-500"
+          class="rounded-lg border px-4 py-2 text-xl font-semibold hover:bg-white hover:text-black"
           @click="letterClick = true"
         >
           Guess Letter
         </button>
         <UiModal v-model="letterClick" title="Guess Letter">
-          <label for="letterGuess">letter</label>
-          <input type="text" />
+          <form @submit.prevent="guessLetter">
+            <label for="letter-guess">letter</label>
+            <input type="text" name="letter-guess" />
+            <button>Guess</button>
+          </form>
         </UiModal>
       </div>
       <div>
         <button
-          class="rounded-full bg-blue-600 px-4 py-2 text-xl font-semibold hover:bg-blue-500"
+          class="rounded-lg border px-4 py-2 text-xl font-semibold hover:bg-white hover:text-black"
           @click="wordClick = true"
         >
           Guess Word
         </button>
         <UiModal v-model="wordClick" title="Guess Word">
-          <label for="letterGuess">Word</label>
-          <input type="text"
-        /></UiModal>
+          <form @submit.prevent="guessWord">
+            <label for="word-guess">Word</label>
+            <input type="text" name="word-guess" />
+            <button>Guess</button>
+          </form>
+        </UiModal>
       </div>
       <button
-        class="rounded-full bg-blue-600 px-4 py-2 text-xl font-semibold hover:bg-blue-500"
+        class="rounded-lg border px-4 py-2 text-xl font-semibold hover:bg-white hover:text-black"
         @click="skipTurn"
       >
         Skip Turn
@@ -42,7 +48,24 @@ const { $io } = useNuxtApp();
 const letterClick = ref(false);
 const wordClick = ref(false);
 
-const guessLetter = () => {};
-const guessWord = () => {};
-const skipTurn = () => {};
+const guessedLetter = ref("");
+const guessedWord = ref("");
+
+const guessLetter = () => {
+  //get the guessed letter
+  //send the guessed letter to the server
+  $io.emit(ClientEvents.GUESS_LETTER, guessedLetter.value);
+  //close the modal
+  letterClick.value = false;
+  guessedLetter.value = "";
+};
+const guessWord = () => {
+  $io.emit(ClientEvents.GUESS_WORD, guessedWord.value);
+  //close the modal
+  wordClick.value = false;
+  guessedWord.value = "";
+};
+const skipTurn = () => {
+  $io.emit(ClientEvents.SKIP_TURN);
+};
 </script>
