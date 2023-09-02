63501be4e3b80735e0b09e9595939a706acc00bf
kadeem-lewis <print.helloworld@genericindividual.com>
2023-09-02 18:11:13 -0400

    updated styling and look of pages and components

--- packages/client/pages/mode.vue
+++ packages/client/pages/mode.vue
@@ -1,24 +1,30 @@
 <template>
-  <h2 class="text-center text-2xl font-bold">Choose Mode</h2>
-  <div class="flex flex-col gap-4">
-    <button
-      @click="createGame"
-      class="btn mx-auto mt-2 w-3/4 rounded-lg py-2 text-xl font-semibold"
-    >
-      Create Game
-    </button>
-    <input
-      type="text"
-      v-model="gameCode"
-      placeholder="Enter Room Code"
-      class="mx-2 rounded-md bg-dark-mode-400 px-4 py-2 outline-none"
-    />
-    <button
-      @click="joinGame"
-      class="btn mx-auto w-3/4 py-2 text-xl font-semibold"
-    >
-      Join Game
-    </button>
+  <div class="m-auto max-w-4xl">
+    <h2 class="text-center text-2xl font-bold">Choose Mode</h2>
+    <div class="grid gap-8 lg:grid-cols-2">
+      <div class="flex items-end justify-center rounded-lg border p-8">
+        <button
+          @click="createGame"
+          class="btn rounded-lg py-2 text-xl font-semibold"
+        >
+          Create Game
+        </button>
+      </div>
+      <div class="flex flex-col justify-center gap-4 rounded-lg border p-8">
+        <input
+          type="text"
+          v-model="gameCode"
+          placeholder="Enter Room Code"
+          class="mx-2 rounded-md bg-dark-mode-400 px-4 py-2 outline-none"
+        />
+        <button
+          @click="joinGame"
+          class="btn mx-auto py-2 text-xl font-semibold"
+        >
+          Join Game
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
