b0b8d6640714fd872c9d4eee66e42d9dcb056782
kadeem-lewis <email@kadeemlewis.com>
2024-07-24 01:56:28 -0400

    used vueuse to handle setting and retrieving localstorage values

--- packages/client/pages/index.vue
+++ packages/client/pages/index.vue
@@ -46,21 +46,19 @@ import { Icon } from "@iconify/vue";
 const { $io } = useNuxtApp();
 import { createAvatar } from "@dicebear/core";
 import { adventurerNeutral } from "@dicebear/collection";
+import { useStorage } from "@vueuse/core";
 
-const username = ref("");
-const userId = ref("");
+const username = useStorage("username", "");
 const avatar = ref("");
-const seed = ref("");
+const seed = useStorage("userSeed", "");
+
+
 
 onMounted(() => {
-  if (localStorage.getItem("username") !== null) {
-    username.value = localStorage.getItem("username") as string;
-  }
+  generateAvatar();
 });
 
 const register = () => {
-  localStorage.setItem("username", username.value);
-  localStorage.setItem("userSeed", seed.value);
   $io.auth = { username: username.value, avatar: avatar.value };
   $io.connect();
   navigateTo({ path: "/mode" });
@@ -78,10 +76,5 @@ const changeAvatar = () => {
   seed.value = Math.floor(Math.random() * 100).toString();
   generateAvatar();
 };
-onMounted(() => {
-  if (localStorage.getItem("userSeed") !== null) {
-    seed.value = localStorage.getItem("userSeed") as string;
-  }
-  generateAvatar();
-});
+
 </script>
