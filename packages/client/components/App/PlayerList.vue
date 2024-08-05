bf793d84710a266fe7b41925d517cf44a7c8fcb5
kadeem-lewis <email@kadeemlewis.com>
2024-08-05 04:28:09 -0400

    updated rendering of player list and added placeholder section for missing players

--- packages/client/components/App/PlayerList.vue
+++ packages/client/components/App/PlayerList.vue
@@ -1,59 +1,60 @@
 <template>
   <div v-if="players" class="flex flex-col gap-y-2">
-    <UCard v-for="(player, key) in players" :key="key" class="text-center">
+    <UCard v-for="[key, player] of players" :key="key" class="text-center">
       <div
-        v-if="players[key]"
         class="flex flex-row items-center justify-between"
         @click="() => (isOpen = true)"
       >
         <div>
           <div class="font-bold">#{{ "1" }}</div>
-          <UIcon v-if="players[key].isHost" name="i-mdi-crown" />
+          <UIcon v-if="player.isHost" name="i-mdi-crown" />
         </div>
         <div>
           <div>
             <span class="font-bold">
-              {{ players[key].username }}
+              {{ player.username }}
             </span>
             <span v-if="$io.id === key"> ( You ) </span>
           </div>
           <div>{{ 0 }} points</div>
         </div>
         <UAvatar
-          :src="players[key].avatar"
-          :name="`${players[key].username} Avatar`"
+          :src="player.avatar"
+          :name="`${player.username} Avatar`"
           size="sm"
         />
         <UModal v-model="isOpen"> Hey hey hey </UModal>
-      </div>
-      <div v-else class="flex justify-around">
+      </div> </UCard
+    ><UCard v-for="item in emptySlots" :key="item">
+      <div class="flex justify-around">
         <span
           class="flex h-9 w-9 items-center justify-center overflow-hidden rounded-full bg-dark-mode-400"
         >
-          <UIcon name="i-heroicons-user" /></span
-        ><span>Empty</span>
+          <UIcon name="i-heroicons-user" />
+        </span>
+        <span>Empty</span>
       </div>
     </UCard>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { User } from "@hangmen/shared";
-
 const { $io } = useNuxtApp();
 
-const players = useState<{ [id: string]: User } | null>("players");
+const { players } = storeToRefs(useRoomStore());
+
+const emptySlots = computed(() => 4 - players.value.size);
 
 onMounted(() => {
   //TODO: param is currently username because userId isnt properly setup yet. change back when done because two users with same username would break system
-  $io.on(ServerEvents.PLAYER_LEAVE_ROOM, ({ username }) => {
-    if (players.value) {
-      Object.keys(players.value).forEach((key) => {
-        if (players.value && players.value[key].username === username) {
-          delete players.value[key];
-        }
-      });
-    }
+  $io.on(ServerEvents.PLAYER_LEAVE_ROOM, (_, playerList) => {
+    players.value = new Map(playerList);
+  });
+});
+
+onMounted(() => {
+  $io.on(ServerEvents.NEW_PLAYER, (_, playerList) => {
+    players.value = new Map(playerList);
   });
 });
 
@@ -61,6 +62,7 @@ const isOpen = ref(false);
 
 onBeforeUnmount(() => {
   $io.off(ServerEvents.PLAYER_LEAVE_ROOM);
-  players.value = null; //player array isn't emptied for room creator
+  $io.off(ServerEvents.NEW_PLAYER);
+  players.value.clear; //player array isn't emptied for room creator
 });
 </script>
