c57dbb7bcfc9a2b0bf43cca53eee4885bc391f28
kadeem-lewis <email@kadeemlewis.com>
2024-08-08 01:43:14 -0400

    Added game start and guess handling logic to gameHandler.ts

--- packages/server/src/handlers/GameHandler.ts
+++ packages/server/src/handlers/GameHandler.ts
@@ -4,30 +4,75 @@ import {
   ServerEvents,
   ClientPayloads,
   ServerPayloads,
+  InterServerEvents,
+  SocketData,
 } from "@hangmen/shared";
 import { activeRooms } from "./RoomHandler.js";
 
 export const gameHandler = (
-  io: Server<ClientPayloads, ServerPayloads>,
-  socket: Socket<ClientPayloads, ServerPayloads>
+  io: Server<ClientPayloads, ServerPayloads, InterServerEvents, SocketData>,
+  socket: Socket<ClientPayloads, ServerPayloads, InterServerEvents, SocketData>
 ) => {
-  socket.on(ClientEvents.START_GAME, (settings, callback) => {
+  socket.on(ClientEvents.START_GAME, async (settings, callback) => {
     const room = activeRooms[socket.data.roomId];
 
-    room.startGame(settings);
+    console.log("AT GAME START:", socket.data);
+
+    await room.startGame(settings);
 
     if (room.word) {
       //this callback only sends it to the socket that started the game. Callback should probably only let user know that start game didnt fail
+      console.log("word: ", room.word);
       callback({
         status: "ok",
         word: room.maskedWord,
         category: room.word.category,
       });
+      io.in(room.code).emit(
+        ServerEvents.GAME_START,
+        room.maskedWord,
+        room.word.category,
+        room.currentGuesser!
+      );
     }
-    socket.on(ClientEvents.SEND_GUESS, (guess) => {
-      room.guessLetter(guess);
+  });
+  socket.on(ClientEvents.SEND_GUESS, (guess) => {
+    const room = activeRooms[socket.data.roomId];
+    //this needs to also track who is the current guesser
+    const score = room.guessLetter(guess);
+    // I also need to get the number of times the letter was in the word
+    if (score > 0) {
+      const currentScore = room.scores.get(socket.id) ?? 0;
+      room.scores.set(socket.id, currentScore + score);
+      socket.data.points = room.scores.get(socket.id)!;
+    }
+    if (room.getPlayers().size > 1) socket.data.isGuesser = false;
+    room.nextTurn();
 
-      io.in(socket.data.roomId).emit(ServerEvents.GAME_UPDATE, room.maskedWord);
-    });
+    // add guessed letters to data sent
+    io.in(socket.data.roomId).emit(
+      ServerEvents.GAME_UPDATE,
+      room.maskedWord,
+      Array.from(room.guessedLetters),
+      room.currentGuesser!,
+      Array.from(room.getPlayers())
+    );
+  });
+  socket.on(ClientEvents.SKIP_TURN, () => {
+    const room = activeRooms[socket.data.roomId];
+    room.nextTurn();
+
+    if (room.getPlayers().size > 1) socket.data.isGuesser = false;
+
+    console.log("Socket Data:", socket.data);
+    console.log("Player Data:", room.players.get(socket.id));
+
+    io.in(socket.data.roomId).emit(
+      ServerEvents.GAME_UPDATE,
+      room.maskedWord,
+      Array.from(room.guessedLetters),
+      room.currentGuesser!,
+      Array.from(room.getPlayers())
+    );
   });
 };
