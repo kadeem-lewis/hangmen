9898b06092a10880f27f541766ae3f15c997453f
kadeem-lewis <email@kadeemlewis.com>
2024-08-08 00:53:15 -0400

    updated word helper to convert word to lowercase before masking

--- packages/server/src/utils/WordHelper.ts
+++ packages/server/src/utils/WordHelper.ts
@@ -1,5 +1,9 @@
-export function maskWord(word: string, guessedLetters: Set<string>): string[] {
+export function maskWord(
+  word: string,
+  guessedLetters: Set<string> = new Set()
+): string[] {
   return word
+    .toLowerCase()
     .split("")
     .map((letter) => (guessedLetters.has(letter) ? letter : "_"));
 }
